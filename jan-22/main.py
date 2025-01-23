bb3fb3932e0e049348ed9192ee4f66bd53f4ab8d
ray <me@rayhanadev.com>
2025-01-23 02:57:24 -0500

    feat: add assignment 01

---
+++ jan-22/main.py
@@ -0,0 +1,41 @@
+#=========================================================================
+# Name & Lab Section: Rayhan Noufal Arayilakath, Friday 7:30 AM
+# Purdue Email: rayhan@purdue.edu
+# Program Description: This program outputs text to the console.
+# Academic Honesty:
+# I attest that this is my original work.
+# I have not used unauthorized source code, either modified or unmodified.
+# I have not given other fellow student(s) access to my program.
+#=========================================================================
+
+def main():
+    output = f"""
+\t================================================
+\t*        Name: Rayhan Noufal Arayilakath       *
+\t*           Email: rayhan@purdue.edu           *
+\t*             CNIT155-Assignment 01            *
+\t================================================
+
+\t CCCCCCCCC         IIIIIIIIII         IIIIIIIIII
+\tCC                     II                 II
+\tCC                     II                 II
+\tCC                     II                 II
+\tCC                     II                 II
+\tCC                     II                 II
+\t CCCCCCCCC         IIIIIIIIII             II
+
+
+
+\t    11              555555555          555555555
+\t  1111             55                 55
+\t    11             55                 55
+\t    11             5555555555         5555555555
+\t    11                     55                 55
+\t    11                     55                 55
+\t1111111111         555555555          555555555
+    """
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
